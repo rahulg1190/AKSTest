078322e1f526afba577f0215d1b8d66f5d4a3242
rahulg1190 <rahulg1190@gmail.com>
2021-03-12 16:07:53 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM mcr.microsoft.com/dotnet/framework/aspnet:4.8-windowsservercore-ltsc2019
+RUN Invoke-WebRequest https://download.microsoft.com/download/0/1/D/01DC28EA-638C-4A22-A57B-4CEF97755C6C/WebDeploy_amd64_en-US.msi -OutFile c:\webdeploy.msi; \
+ Start-Process -FilePath 'c:\webdeploy.msi' -ArgumentList '/quiet', '/NoRestart', '/passive', 'ADDLOCAL=ALL', 'LicenseAccepted="0"' -Wait; \
+ Remove-Item c:\webdeploy.msi -Force; \
+ Set-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem\' -name LongPathsEnabled -value 1;
+COPY ["./MandatoryArtifacts.zip", "C:/AppModnzData/MandatoryArtifacts.zip"]
+COPY ./Entryscript.ps1 c:/Entryscript.ps1
+ENTRYPOINT powershell c:/Entryscript.ps1
+EXPOSE 3552
---
+++ Entryscript.ps1
@@ -0,0 +1,37 @@
+try {
+$msdeployPath = " C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe";
+$setParams = "";
+Expand-Archive -Path 'C:\AppModnzData\MandatoryArtifacts.zip' -DestinationPath 'C:\AppModnzData\MandatoryArtifacts' -ErrorAction Stop
+$configs = Get-Content -Raw -Path 'C:\AppSecrets\Application.config' -ErrorAction Stop | Out-String | ConvertFrom-Json
+foreach ($config in $configs)
+{
+  $setParams += " -setParam:name=" + $config.Identifier + ",value='" + $config.Value + "'"
+}
+$msdeployExpression = "&'$msdeployPath' --% -verb:sync -source:archiveDir='C:\AppModnzData\MandatoryArtifacts' -dest:auto $setParams";
+Invoke-Expression $msdeployExpression
+if ($LASTEXITCODE -ne 0) { throw 'Failed to deploy the application using web deploy' }
+
+###### Updating ACLs for the artifacts ######
+$paths = @('C:\parts')
+ForEach ($path in $paths)
+{
+	$acl = Get-ACL "$path"
+	Set-ACL -Path "$path" -ACLObject $acl
+	$acl = Get-ACL "$path"
+	$acl.AddAccessRule($(New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users", "Modify, Synchronize", "None", "None", "Allow")))
+	$acl.AddAccessRule($(New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Users", "ReadAndExecute, Synchronize", "None", "None", "Allow")))
+	if ($(Test-Path -Path $path -PathType Container))
+	{
+		$acl.AddAccessRule($(New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users", "Modify, Synchronize", "ContainerInherit, ObjectInherit", "InheritOnly", "Allow")))
+		$acl.AddAccessRule($(New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Users", "ReadAndExecute, Synchronize", "ContainerInherit, ObjectInherit", "InheritOnly", "Allow")))
+	}
+	Set-ACL -Path "$path" -ACLObject $acl
+}
+
+Remove-Item -Path 'C:\AppModnzData\' -Recurse -Force
+}
+catch {
+ echo $_.Exception
+ throw 'Failed to setup the application'
+}
+.\ServiceMonitor.exe w3svc
